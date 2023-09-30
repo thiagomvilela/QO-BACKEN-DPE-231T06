257aee51b437b6e43d67437797dfdd307dff10ab
Thiago Vilela <thiago.vilela@outlook.com.br>
2023-09-30 20:17:54 -0300

    04c - Atividade 3 - Criar estrutura da API com Context e método GET para projetos, teste no navegador

---
+++ Exo.WebApi/Contexts/ExoContext.cs
@@ -0,0 +1,35 @@
+using Exo.WebApi.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Data.SqlClient;
+
+namespace Exo.WebApi.Contexts
+{
+    public class ExoContext : DbContext
+    {
+        public ExoContext()
+        {
+        }
+
+        public ExoContext(DbContextOptions<ExoContext> options) : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                // Essa string de conexão depende da SUA máquina.
+                optionsBuilder.UseSqlServer("Server=localhost\\SQLEXPRESS;Database=ExoApi;Trusted_Connection=True;");
+
+                // Se você quiser usar o exemplo 1 ou 2, certifique-se de que eles também estejam corretos.
+                // Exemplo 1 de string de conexão:
+                // "User ID=sa;Password=1234;Server=localhost;Database=ExoApi;Trusted_Connection=False;"
+
+                // Exemplo 2 de string de conexão:
+                // "Server=localhost\\SQLEXPRESS;Database=ExoApi;Trusted_Connection=True;"
+            }
+        }
+
+        public DbSet<Projeto> Projetos { get; set; }
+    }
+}
