4b126dbc26bab7b22e2e37f4aec50e5e7021e706
Thiago Vilela <thiago.vilela@outlook.com.br>
2023-09-30 20:45:20 -0300

    04d - Atividade 4 - Criar o restante do CRUD de projetos, testando-o no Insomnia

--- Exo.WebApi/Controllers/ProjetosController.cs
+++ Exo.WebApi/Controllers/ProjetosController.cs
@@ -9,10 +9,9 @@ namespace Exo.WebApi.Controllers
     [ApiController]
     public class ProjetosController : ControllerBase
     {
-        private readonly ProjetoRepository
-            _projetoRepository;
+        private readonly ProjetoRepository _projetoRepository;
         public ProjetosController(ProjetoRepository
-            projetoRepository)
+        projetoRepository)
         {
             _projetoRepository = projetoRepository;
         }
@@ -20,6 +19,42 @@ namespace Exo.WebApi.Controllers
         public IActionResult Listar()
         {
             return Ok(_projetoRepository.Listar());
+        }
+        // Código novo que completa o CRUD.
+        [HttpPost]
+        public IActionResult Cadastrar(Projeto projeto)
+        {
+            _projetoRepository.Cadastrar(projeto);
+            return StatusCode(201);
+        }
+        [HttpGet("{id}")]
+        public IActionResult BuscarPorId(int id)
+        {
+            Projeto projeto = _projetoRepository.BuscarporId(id);
+            if (projeto == null)
+            {
+                return NotFound();
+            }
+            return Ok(projeto);
+        }
+        [HttpPut("{id}")]
+        public IActionResult Atualizar(int id, Projeto projeto)
+        {
+            _projetoRepository.Atualizar(id, projeto);
+            return StatusCode(204);
+        }
+        [HttpDelete("{id}")]
+        public IActionResult Deletar(int id)
+        {
+            try
+            {
+                _projetoRepository.Deletar(id);
+                return StatusCode(204);
+            }
+            catch (Exception e)
+            {
+                return BadRequest();
+            }
         }
     }
 }
